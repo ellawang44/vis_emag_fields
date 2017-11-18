088681931ec2a37f2a5cc0264cecf4b6b7e359ba
Xi Wang <u6083620@anu.edu.au>
2017-11-18 14:51:38 +0800

    added draw infinite wire and torus. Added magnetic vector field of infinite wire.

--- functions.py
+++ functions.py
@@ -4,6 +4,7 @@ import mayavi.mlab as mplt
 
 ep_0 = 8.854*10**-12 # vaccumn permissitivity
 cons = 1/(4*np.pi*ep_0) # Coloumb's constant (k)
+mu_0 = 4 * np.pi * 10**(-7) # permeability of free space
 
 def e_calc(q, r, r_prime):
 
@@ -18,9 +19,9 @@ def e_calc(q, r, r_prime):
     if d_sq == 0.0:
         return (0.,0.,0.)
     else:
-        E_x = cons*q*x_dist/d_sq
-        E_y = cons*q*y_dist/d_sq
-        E_z = cons*q*z_dist/d_sq
+        E_x = cons * q * x_dist / d_sq
+        E_y = cons * q * y_dist / d_sq
+        E_z = cons * q * z_dist / d_sq
         return (E_x, E_y, E_z)
 
 def sphere(q, pos):
@@ -48,7 +49,7 @@ def e_vector(q, pos, x_grid, y_grid, z_grid, x_field, y_field, z_field):
         sphere(charge, location)
     # draw vector field
     X,Y,Z = np.meshgrid(x_grid, y_grid, z_grid, indexing = 'ij')
-    mplt.quiver3d(X,Y,Z, x_field, y_field, z_field)
+    mplt.quiver3d(X, Y, Z, x_field, y_field, z_field)
     # set view to x-axis coming out of screen
     fig.scene.x_plus_view()
     mplt.show()
@@ -60,19 +61,130 @@ def e_field(q, pos, x_grid, y_grid, z_grid, x_field, y_field, z_field, no_lines)
         # draw sphere for point charge
         sphere(charge, location)
         # draw electric field lines
-        thing = mplt.flow(X,Y,Z,x_field, y_field, z_field, figure=fig, seedtype='sphere', integration_direction='both')
-        thing.seed.widget.center = location
+        ball = mplt.flow(X,Y,Z,x_field, y_field, z_field, figure=fig, seedtype='sphere', integration_direction='both')
+        ball.seed.widget.center = location
         # number of field lines to integrate over
-        thing.seed.widget.theta_resolution = no_lines
-        thing.seed.widget.phi_resolution = no_lines
+        ball.seed.widget.theta_resolution = no_lines
+        ball.seed.widget.phi_resolution = no_lines
         # number of integration steps
-        thing.stream_tracer.maximum_propagation = 200
-        thing.seed.widget.radius = 1
-        # dodgy hax...
-        thing.seed.widget.enabled = False
-        thing.seed.widget.enabled = True
+        ball.stream_tracer.maximum_propagation = 200
+        ball.seed.widget.radius = 1
+        # dodgy hax... TL;DR widgets are dumb
+        ball.seed.widget.enabled = False
+        ball.seed.widget.enabled = True
     mplt.axes()
     # set view to x-axis coming out of screen
     fig.scene.x_plus_view()
     mplt.draw(figure=fig)
     mplt.show()
+
+def torus(R):
+
+    ''' draw torus for magnetic field of coil'''
+
+    # thickness of the torus
+    thickness = 1
+    # polar coordinates for torus
+    u = np.linspace(0,2*np.pi,100)
+    v = np.linspace(0,2*np.pi,100)
+    u,v = np.meshgrid(u,v)
+    # convert to cartesian coordinates
+    x = (R + thickness * np.cos(v)) * np.cos(u)
+    y = (R + thickness * np.cos(v)) * np.sin(u)
+    z = thickness * np.sin(v)
+    mplt.mesh(x, y, z, color = (0.3, 0.3, 0.3))
+
+def wire(ori, loc, grid):
+    s = 0.5
+    phi = np.linspace(0, 2*np.pi, 100)
+    phi, _ = np.meshgrid(phi, phi)
+    grid = grid.split()
+    grid = [int(i) for i in grid]
+    if ori == 'x':
+        x, _ = np.mgrid[grid[0]:grid[1]:100j, grid[0]:grid[1]:100j]
+        y = s*np.cos(phi) + loc[1]
+        z = s*np.sin(phi) + loc[2]
+    elif ori == 'y':
+        x = s*np.cos(phi) + loc[0]
+        y, _ = np.mgrid[grid[3]:grid[4]:100j, grid[6]:grid[7]:100j]
+        z = s*np.sin(phi) + loc[2]
+    elif ori == 'z':
+        x = s*np.cos(phi) + loc[0]
+        y = s*np.sin(phi) + loc[1]
+        z, _ = np.mgrid[grid[6]:grid[7]:100j, grid[6]:grid[7]:100j]
+    else:
+        raise ValueError('not an allowed orientation')
+    mplt.mesh(x, y, z, color = (1, 0, 0))
+
+def m_calc_wire(I, ori, loc, r):
+
+    '''calculate magnetic field of an infinite wire'''
+
+    x = r[0]
+    y = r[1]
+    z = r[2]
+    if ori == 'x':
+        num = (y-loc[1])**2 + (z-loc[2])**2
+        if num == 0.0:
+            return (0., 0., 0.)
+        cons = mu_0 * I / (2 * np.pi * num)
+        B_x = 0.0
+        B_y = cons * -(z - loc[2])
+        B_z = cons * (y - loc[1])
+    elif ori == 'y':
+        num = (x-loc[0])**2 + (z-loc[2])**2
+        if num == 0.0:
+            return (0., 0., 0.)
+        cons = mu_0 * I / (2 * np.pi * num)
+        B_x = cons * (z - loc[2])
+        B_y = 0.0
+        B_z = cons * -(x - loc[0])
+    elif ori == 'z':
+        num = (x-loc[0])**2 + (y-loc[1])**2
+        if num == 0.0:
+            return (0., 0., 0.)
+        cons = mu_0 * I / (2 * np.pi * num)
+        B_x = cons * -(y - loc[1])
+        B_y = cons * (x - loc[0])
+        B_z = 0.0
+    else:
+        raise ValueError('not an allowed orientation')
+    return (B_x, B_y, B_z)
+
+def m_vector_wire(ori, loc, grid, x_grid, y_grid, z_grid, x_field, y_field, z_field):
+    fig = mplt.figure()
+    # draw sphere for point charge
+    for orientation, location in zip(ori, loc):
+        wire(orientation, location, grid)
+    # draw vector field
+    X,Y,Z = np.meshgrid(x_grid, y_grid, z_grid, indexing = 'ij')
+    mplt.quiver3d(X, Y, Z, x_field, y_field, z_field)
+    # set view to x-axis coming out of screen
+    fig.scene.x_plus_view()
+    mplt.show()
+
+''' this doesn't work
+def m_field_wire(q, pos, x_grid, y_grid, z_grid, x_field, y_field, z_field, no_lines):
+    fig = mplt.figure()
+    X,Y,Z = np.meshgrid(x_grid, y_grid, z_grid, indexing = 'ij')
+    for charge, location in zip(q, pos):
+        # draw sphere for point charge
+        sphere(charge, location)
+        # draw electric field lines
+        ball = mplt.flow(X,Y,Z,x_field, y_field, z_field, figure=fig, seedtype='sphere', integration_direction='both')
+        ball.seed.widget.center = location
+        # number of field lines to integrate over
+        ball.seed.widget.theta_resolution = no_lines
+        ball.seed.widget.phi_resolution = no_lines
+        # number of integration steps
+        ball.stream_tracer.maximum_propagation = 200
+        ball.seed.widget.radius = 1
+        # dodgy hax... TL;DR widgets are dumb
+        ball.seed.widget.enabled = False
+        ball.seed.widget.enabled = True
+    mplt.axes()
+    # set view to x-axis coming out of screen
+    fig.scene.x_plus_view()
+    mplt.draw(figure=fig)
+    mplt.show()
+    '''
--- main.py
+++ main.py
@@ -12,6 +12,8 @@ parser.add_option('-f', '--file', type = 'string', dest = 'file', help = 'Reads 
 # draw different graphs
 parser.add_option('--e_vector', action = 'store_true', dest = 'e_vector', help = 'Draw electric vector field.')
 parser.add_option('--e_field', action = 'store_true', dest = 'e_field', help = 'Draw electric field.')
+parser.add_option('--m_vector', action = 'store_true', dest = 'm_vector', help = 'Draw magnetic vector field.')
+parser.add_option('--m_field', action = 'store_true', dest = 'm_field', help = 'Draw magnetic field.')
 # changing grid size, input should look something like:
 # 15 15 1 10 10 1 15 15 1
 parser.add_option('-g', '--grid', type = 'string', dest = 'grid', help = 'Input render grid.')
@@ -21,19 +23,34 @@ parser.add_option('--lines', type = 'int', dest = 'lines', help = 'Changes the n
 (options, args) = parser.parse_args()
 
 # set file name
-name = 'pos_charge' # default
+if options.e_vector or options.e_field:
+    name = 'pos_charge' # default
+elif options.m_vector or options.m_field:
+    name = 'inf_wire' # default
 if options.file:
     name = options.file
 
 # read in csv file where the charges should be defined
-charge = []
-pos = []
-with open('data/' + name + '.csv') as f:
-    reader = csv.reader(f, delimiter=',')
-    next(reader)
-    for row in reader:
-        charge.append(float(row[0]))
-        pos.append((float(row[1]), float(row[2]), float(row[3])))
+if options.e_vector or options.e_field:
+    charge = []
+    pos = []
+    with open('data/' + name + '.csv') as f:
+        reader = csv.reader(f, delimiter=',')
+        next(reader)
+        for row in reader:
+            charge.append(float(row[0]))
+            pos.append((float(row[1]), float(row[2]), float(row[3])))
+elif options.m_vector or options.m_field:
+    currents = []
+    orientations = []
+    pos= []
+    with open('data/' + name + '.csv') as f:
+        reader = csv.reader(f, delimiter = ',')
+        next(reader)
+        for row in reader:
+            currents.append(float(row[0]))
+            orientations.append(row[1])
+            pos.append((float(row[2]), float(row[3]), float(row[4])))
 
 # changes grid size
 if options.grid:
@@ -43,6 +60,7 @@ if options.grid:
     y_grid = np.arange(grid[3], grid[4] + grid[5], grid[5])
     z_grid = np.arange(grid[6], grid[7] + grid[8], grid[8])
 else:
+    grid = '-10 10 1 -10 10 1 -10 10 1'
     x_grid = np.arange(-10, 11, 1)
     y_grid = np.arange(-10, 11, 1)
     z_grid = np.arange(-10, 11, 1)
@@ -60,15 +78,16 @@ for x in x_grid:
         y_field_y_z = []
         z_field_y_z = []
         for z in z_grid:
-            elec = np.sum(np.array([functions.e_calc(q, (x, y, z), p) for q, p in zip(charge, pos) if functions.e_calc(q, (x, y, z), p) is not None]), axis = 0)
-            if elec is None:
-                x_field_y_z.append(0)
-                y_field_y_z.append(0)
-                z_field_y_z.append(0)
-            else:
-                x_field_y_z.append(elec[0])
-                y_field_y_z.append(elec[1])
-                z_field_y_z.append(elec[2])
+            # calculate electric field
+            if options.e_vector or options.e_field:
+                val = np.sum(np.array([functions.e_calc(q, (x, y, z), p) for q, p in zip(charge, pos)]), axis = 0)
+            # calculate magnetic field
+            elif options.m_vector or options.m_field:
+                val = np.sum(np.array([functions.m_calc_wire(i, o, l, (x, y, z)) for i, o, l in zip(currents, orientations, pos)]), axis = 0)
+            # append values
+            x_field_y_z.append(val[0])
+            y_field_y_z.append(val[1])
+            z_field_y_z.append(val[2])
         x_field_y.append(x_field_y_z)
         y_field_y.append(y_field_y_z)
         z_field_y.append(z_field_y_z)
@@ -89,6 +108,7 @@ if options.e_vector:
     functions.e_vector(charge, pos, x_grid, y_grid, z_grid, x_field, y_field, z_field)
 elif options.e_field:
     functions.e_field(charge, pos, x_grid, y_grid, z_grid, x_field, y_field, z_field, no_lines)
-else:
-    functions.e_vector(charge, pos, x_grid, y_grid, z_grid, x_field, y_field, z_field)
-    functions.e_field(charge, pos, x_grid, y_grid, z_grid, x_field, y_field, z_field, no_lines)
+elif options.m_vector:
+    functions.m_vector_wire(orientations, pos, grid, x_grid, y_grid, z_grid, x_field, y_field, z_field)
+elif options.m_field:
+    functions.m_field_wire(ori, loc, grid, x_grid, y_grid, z_grid, x_field, y_field, z_field, no_lines)
